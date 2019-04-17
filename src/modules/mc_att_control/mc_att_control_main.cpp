b7476d343897f40183a7bb7cee2f5b0611c7c789
Beat Küng <beat-kueng@gmx.net>
2019-04-17 11:52:08 +0200

    mc rate controller: add I term reduction factor
    Reduce the I gain for high rate errors to reduce bounce-back effects after
    flips. Up to 200 degrees the gain is almost not reduced (<25%), so this
    will only take noticeable effects for large errors (setpoint changes),
    where we actually want to have an effect.
    
    This allows to increase the MC_*RATE_I parameters w/o negative effects
    when doing flips (i.e. bounce-back after flips).
    
    The 400 degrees limit and the x^2 are empirical.
    
    The better the rate tracking in general (high P gain), the less this is
    required (because of the lower tracking error). At the same time it also
    does not harm, as the i_factor will always be close to 1.

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -481,8 +481,12 @@ MulticopterAttitudeControl::control_attitude_rates(float dt)
 
 			}
 
+			// I term factor: reduce the gain with increasing rate error (to avoid bounce-back after flips)
+			float i_factor = rates_err(i) / math::radians(400.f);
+			i_factor = math::max(0.0f, 1.f - i_factor * i_factor);
+
 			// Perform the integration using a first order method and do not propagate the result if out of range or invalid
-			float rate_i = _rates_int(i) + rates_i_scaled(i) * rates_err(i) * dt;
+			float rate_i = _rates_int(i) + i_factor * rates_i_scaled(i) * rates_err(i) * dt;
 
 			if (PX4_ISFINITE(rate_i) && rate_i > -_rate_int_lim(i) && rate_i < _rate_int_lim(i)) {
 				_rates_int(i) = rate_i;
